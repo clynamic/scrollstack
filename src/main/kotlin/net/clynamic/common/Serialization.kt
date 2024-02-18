c672c65ea3f7d6fd3801a65da297532e1997e7f0
clragon <clragon@clynamic.net>
2024-02-18 05:35:59 +0100

    fixed returning ids in creation

--- src/main/kotlin/net/clynamic/common/Serialization.kt
+++ src/main/kotlin/net/clynamic/common/Serialization.kt
@@ -4,10 +4,17 @@ import com.fasterxml.jackson.core.util.DefaultIndenter
 import com.fasterxml.jackson.core.util.DefaultPrettyPrinter
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import io.ktor.http.ContentType
+import io.ktor.http.content.OutgoingContent
+import io.ktor.http.content.TextContent
+import io.ktor.serialization.ContentConverter
 import io.ktor.serialization.jackson.jackson
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.util.reflect.TypeInfo
+import io.ktor.utils.io.ByteReadChannel
+import io.ktor.utils.io.charsets.Charset
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
@@ -17,8 +24,35 @@ fun Application.configureSerialization() {
                 indentArraysWith(DefaultPrettyPrinter.FixedSpaceIndenter.instance)
                 indentObjectsWith(DefaultIndenter("  ", "\n"))
             })
+            disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
             registerModule(JavaTimeModule())
             disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
         }
+        register(ContentType.Text.Plain, IdTextContentConverter())
     }
+}
+
+
+class IdTextContentConverter : ContentConverter {
+    override suspend fun serializeNullable(
+        contentType: ContentType,
+        charset: Charset,
+        typeInfo: TypeInfo,
+        value: Any?,
+    ): OutgoingContent? {
+        return when (value) {
+            is Int -> TextContent(value.toString(), contentType)
+            else -> null
+        }
+    }
+
+    override suspend fun deserialize(
+        charset: Charset,
+        typeInfo: TypeInfo,
+        content: ByteReadChannel,
+    ): Any? {
+        // This converter is only used for sending out IDs, so we don't need to deserialize
+        return null
+    }
+
 }
